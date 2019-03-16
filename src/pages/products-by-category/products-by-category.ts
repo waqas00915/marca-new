5dfbff6d8a47b706861fe5e1d90f45614baa0da3
Hassan Ali <hassan@appslab.io>
2019-03-17 00:26:46 +0500

    latestworkingcode

--- src/pages/products-by-category/products-by-category.ts
+++ src/pages/products-by-category/products-by-category.ts
@@ -1,58 +1,66 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
 import { WoocommerceProvider } from '../../providers/woocommerce/woocommerce';
 // import { ProductDetails } from '../product-details/product-details';
 
 @IonicPage({})
 @Component({
-  selector: 'page-products-by-category',
-  templateUrl: 'products-by-category.html',
+    selector: 'page-products-by-category',
+    templateUrl: 'products-by-category.html',
 })
 export class ProductsByCategory {
 
-  WooCommerce: any;
-  products: any[];
-  page: number;
-  category: any;
+    WooCommerce: any;
+    products: any[] = [];
+    page: number;
+    category: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private WP: WoocommerceProvider) {
+    constructor(
+        private zone: NgZone,
+        public events: Events,
+        public navCtrl: NavController, public navParams: NavParams, private WP: WoocommerceProvider) {
+        this.events.subscribe('updateScreen', () => {
+            this.zone.run(() => {
+                // console.log('what?', this.projects);
+            });
+        });
+        this.page = 1;
+        this.category = this.navParams.get("category");
 
-    this.page = 1;
-    this.category = this.navParams.get("category");
+        this.WooCommerce = WP.init();
 
-    this.WooCommerce = WP.init();
+        this.WooCommerce.getAsync("products?category=" + this.category.id).then((data) => {
+            this.products = JSON.parse(data.body);
+            console.log('poty', this.products);
+            this.events.publish('updateScreen')
+        }, (err) => {
+            console.log(err)
+        })
 
-    this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug).then((data) => {
-      console.log(JSON.parse(data.body));
-      this.products = JSON.parse(data.body).products;
-    }, (err) => {
-      console.log(err)
-    })
+    }
 
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ProductsByCategory');
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductsByCategory');
-  }
+    loadMoreProducts(event) {
+        this.page++;
+        console.log("Getting page " + this.page);
+        this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug + "&page=" + this.page).then((data) => {
+            let temp = (JSON.parse(data.body).products);
 
-  loadMoreProducts(event) {
-    this.page++;
-    console.log("Getting page " + this.page);
-    this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug + "&page=" + this.page).then((data) => {
-      let temp = (JSON.parse(data.body).products);
+            this.products = this.products.concat(JSON.parse(data.body).products)
+            console.log(this.products);
+            event.complete();
 
-      this.products = this.products.concat(JSON.parse(data.body).products)
-      console.log(this.products);
-      event.complete();
+            if (temp.length < 10)
+                event.enable(false);
+        })
+    }
 
-      if (temp.length < 10)
-        event.enable(false);
-    })
-  }
-
-  openProductPage(product){
-    this.navCtrl.push('ProductDetails', {"product": product} );
-  }
+    openProductPage(product) {
+        this.navCtrl.push('ProductDetails', { "product": product });
+    }
 
 }
